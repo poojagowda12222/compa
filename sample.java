bff0a549e8f46e769994044c0cdc747c6effcdf5
poojagowda12222 <poojaagowda24.16@gmail.com>
2024-08-22 10:05:31 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,11 @@
+class sample{
+public static void main(string[]args)
+{
+scanner obj=new scanner(system.in);
+system.out.println("enter any number");
+
+int num=obj.nextint();
+obj.close();
+system.out.println("the number entered by user is "+num);
+  }
+}
